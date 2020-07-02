95bb5a129000a8ab331e22aecec6b02a00d9cdd8
cristian andres ladino peña <cristianuue@hotmail.com>
2020-07-01 22:58:27 -0500

    Clase 6.5 - Variables, tipos de datos y cadenas

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+
+$lastName = 'Ladino';
+$name = 'Cristian ' . $lastName;
+$name2 = "Cristian $lastName"
+
+?>
+
 <!doctype html>
 <html lang="en">
 
@@ -21,7 +29,7 @@
         <img id="profile-picture" src="https://ui-avatars.com/api/?name=John+Doe&size=255" alt="">
       </div>
       <div class="col">
-        <h1><?php echo 'Cristian Ladino'; ?></h1>
+        <h1><?php echo $name; ?></h1>
         <h2>PHP Developer</h2>
         <ul>
           <li>Mail: hector@mail.com</li>
