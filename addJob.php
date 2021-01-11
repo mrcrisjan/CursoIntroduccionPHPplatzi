fe61ca2ea92262e1c05a00f4f80e6c180a916211
cristian andres ladino peña <cristianuue@hotmail.com>
2021-01-11 18:00:53 -0500

    Clase 28 - Formularios

---
+++ addJob.php
@@ -0,0 +1,26 @@
+<?php
+var_dump($_GET);
+var_dump($_POST);
+?>
+
+<html>
+    <head>
+        <title>Add Job Form</title>
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B"
+        crossorigin="anonymous">
+         <link rel="stylesheet" href="style.css">
+
+    </head>
+    <body>
+        <form action="addJob.php" method="post">
+            <h1>Add Job</h1>
+            <label for="">Title:</label>
+            <input type="text" name="title"><br/>
+            <label for="">Description:</label>
+            <input type="text" name="description"><br/>
+            <button type="submit">Save</button>
+        </form>
+    </body>
+</html>
