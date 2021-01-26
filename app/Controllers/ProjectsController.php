754e959a2a3748fd151ded252b8157fefb7edcee
cristian andres ladino peña <cristianuue@hotmail.com>
2021-01-25 22:48:37 -0500

    Clase 40 - Extendiendo Templates con Twig

--- app/Controllers/ProjectsController.php
+++ app/Controllers/ProjectsController.php
@@ -14,6 +14,6 @@ class ProjectsController extends BaseController {
             $project->save();
         }
 
-        echo $this->renderHTML('addProject.twig');
+        return $this->renderHTML('addProject.twig');
     }
 }
