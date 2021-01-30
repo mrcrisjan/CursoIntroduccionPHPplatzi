95bd76d24b857320ed7bd87ade1a324072174d57
cristian andres ladino peña <cristianuue@hotmail.com>
2021-01-29 21:42:15 -0500

    Clase 46 - Formulario de Login

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use App\Models\User;
 use Respect\Validation\Validator as v;
+use Zend\Diactoros\Response\RedirectResponse;
 
 class AuthController extends BaseController {
     public function getLogin() {
@@ -12,15 +13,20 @@ class AuthController extends BaseController {
     
     public function postLogin($request) {
         $postData = $request->getParsedBody();
+        $responseMessage = null;
         $user = User::where('email', $postData['email'])->first();
         if($user) {
             if (password_verify($postData['password'], $user->password)) {
-                echo 'Password was verified';
+                return new RedirectResponse('admin');
             } else {
-                echo 'Wrong Password';
+                $responseMessage = 'It seems your email/password is wrong. Please check and retry';
             }
         } else {
-            echo 'Not Found';
+            $responseMessage = 'It seems your email/password is wrong. Please check and retry';
         }
+
+        return $this->renderHTML('login.twig', [
+           'responseMessage' => $responseMessage
+        ]);
     }
 }
